a1be04b4038e321f49588b31354dcba0a0babe55
Fabian <f-hinz@outlook.de>
2023-06-11 12:06:58 +0200

    Adjusted docker compose

--- src/database/database.js
+++ src/database/database.js
@@ -3,9 +3,9 @@ const mySql = require('mysql2');
 module.exports = mySql.createConnection({
   database: 'interval_app',
   // If the app is started in the same network as the mysql container this must be the container name of mysql. Otherwise, its localhost.
-  host: 'mysql-container',
+  host: 'localhost',
   password: 'password',
   // If the app is started in the same network as the mysql container this must be the container port of mysql. Otherwise, it's the docker host port.
-  port: 3306,
+  port: 8081,
   user: 'root',
 });
