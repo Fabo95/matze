678ff4e8da34813b405cce66e40ad61c2eb20b4a
Fabian <f-hinz@outlook.de>
2023-06-17 18:46:15 +0200

    Adjusted ports and host of database.

--- database/database.js
+++ database/database.js
@@ -3,9 +3,9 @@ const mySql = require('mysql2');
 module.exports = mySql.createConnection({
   database: 'interval_app',
   // If the app is started in the same network as the mysql container this must be the container name of mysql. Otherwise, its localhost.
-  host: 'localhost',
+  host: 'mysql-container',
   password: 'password',
   // If the app is started in the same network as the mysql container this must be the container port of mysql. Otherwise, it's the docker host port.
-  port: 8081,
+  port: 3306,
   user: 'root',
 });
