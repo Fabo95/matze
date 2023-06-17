f8e0d03bbf65b9368e971568ab0604cbd2f03ae9
Fabian <f-hinz@outlook.de>
2023-06-17 20:09:15 +0200

    Adjust frontend port to run app on port 3000.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,6 @@
 import { Interval } from 'api/utils/apiTypes';
 
 export const apiGetInterval = async (): Promise<Interval> =>
-  fetch('http://localhost:8080/intervals').then((data) => data.json());
+  fetch('http://localhost:8080/intervals', { cache: 'no-cache' }).then((data) =>
+    data.json()
+  );
