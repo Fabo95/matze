a3ed7e8ee8705be49b764eeb94257f6121c6c58d
Fabian <f-hinz@outlook.de>
2023-06-18 00:19:48 +0200

    Added env variables for db login.

--- database/database.js
+++ database/database.js
@@ -4,8 +4,8 @@ module.exports = mySql.createConnection({
   database: 'interval_app',
   // If the app is started in the same network as the mysql container this must be the container name of mysql. Otherwise, its localhost.
   host: 'mysql-container',
-  password: '73p4K-sFGgHfhpH',
+  password: process.env.DB_PASSWORD,
   // If the app is started in the same network as the mysql container this must be the container port of mysql. Otherwise, it's the docker host port.
   port: 3306,
-  user: 'admin',
+  user: process.env.DB_USER,
 });
