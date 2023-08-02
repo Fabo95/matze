30b59f868de5e79429c756a993be6721723f6a8c
Fabian <f-hinz@outlook.de>
2023-08-02 23:56:09 +0200

    fix(sw): added new version of service worker

--- src/serviceWorker/utils/constants.js
+++ src/serviceWorker/utils/constants.js
@@ -1,3 +1,5 @@
-export const dynamicCacheName = `dynmaic-interval_app_${'v1'}`;
-export const staticCacheName = `static-cache-interval_app_${'v1'}`;
-export const pageCache = `page-cache-interval_app_${'v1'}`;
+const version = 1.1;
+
+export const dynamicCacheName = `dynmaic-interval_app_${version}`;
+export const staticCacheName = `static-cache-interval_app_${version}`;
+export const pageCache = `page-cache-interval_app_${version}`;
