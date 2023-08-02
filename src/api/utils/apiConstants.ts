061bd2e8cba704229d2ffe0e68abd30ffda8a1ab
Fabian <f-hinz@outlook.de>
2023-08-02 22:22:21 +0200

    fix(api): api constant

--- src/api/utils/apiConstants.ts
+++ src/api/utils/apiConstants.ts
@@ -1,3 +1,9 @@
-export const apiBaseUrl = 'http://localhost:8080/';
+export const apiBaseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080/'
+    : 'https://interval-app-api.onrender.com/';
 
-export const authBaseUrl = 'http://localhost:8090/';
+export const authBaseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8090/'
+    : 'https://interval-app-auth.onrender.com/';
