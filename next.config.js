7b0892e4c9fa8dff12c38d0020c3dc65290726ca
Fabian <f-hinz@outlook.de>
2023-10-29 19:35:31 +0100

    feat(package): updated next js and removed unneeded config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
     // Needed because of SWC failure. https://nextjs.org/docs/messages/failed-loading-swc
     swcMinify: false,
   },
